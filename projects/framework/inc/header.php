a1e2d82c97986c418c826e4c0a9241213bc89bd1
Chad Allen <chad@resmarkinteractive.com>
2014-02-01 12:19:12 -0700

    Moved the framework here so we can view it on GitHub pages. Still deciding if this will be the permanent home
    Signed-off-by: Chad Allen <chad@resmarkinteractive.com>

---
+++ projects/framework/inc/header.php
@@ -0,0 +1,27 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<meta name="author" content="Resmark Systems">
+<link rel="alternate" media="only screen and (max-width: 769px)" href="" />
+<link rel="alternate" media="handheld" href="" />
+<link rel="apple-touch-icon" href="/img/icons/icon.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/icon.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/icon@2x.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/icon@2x.png" />
+
+<!-- Styles -->
+<link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="/css/semantic.css">
+<link rel="stylesheet" href="/css/animations.css">
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="/js/modernizer.min.js"></script>
+<script type="text/javascript" src="/js/selectivizr-min.js"></script>
+<script type="text/javascript" src="/js/respond.min.js"></script>
+
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+
+<!-- Google Analytics Tracking -->
